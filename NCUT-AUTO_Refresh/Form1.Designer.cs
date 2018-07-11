977cbbbc0e9e4c92982217d251198892d81a5dbd
InTron@NCUT <zy19970@hotmail.com>
2018-07-11 22:47:24 +0800

    添加项目文件。

---
+++ NCUT-AUTO_Refresh/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace NCUT_AUTO_Refresh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 267);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(540, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(460, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(32, 17);
+            this.toolStripStatusLabel1.Text = "时间";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "2s刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 26);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "3s刷新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 26);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "5s刷新";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(368, 26);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "停止刷新";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(349, 41);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 152);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(198, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(220, 201);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "连接测试";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(328, 201);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 33);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "退出";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 3000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 5000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::NCUT_AUTO_Refresh.Properties.Resources.yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(164, 214);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::NCUT_AUTO_Refresh.Properties.Resources.red;
+            this.pictureBox2.Location = new System.Drawing.Point(124, 214);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NCUT_AUTO_Refresh.Properties.Resources.green;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 214);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(133, 17);
+            this.toolStripStatusLabel3.Text = "copyright © InTron";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 289);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NCUT-AUTO自动刷新";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ NCUT-AUTO_Refresh/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NCUT_AUTO_Refresh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool IsPic = false;
+
+
+        private string GetHttpWebRequest(string url)
+        {
+            HttpWebResponse result;
+            string strHTML = string.Empty;
+            try
+            {
+                Uri uri = new Uri(url);
+                WebRequest webReq = WebRequest.Create(uri);
+                WebResponse webRes = webReq.GetResponse();
+
+                HttpWebRequest myReq = (HttpWebRequest)webReq;
+                myReq.UserAgent = "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705";
+                myReq.Accept = "*/*";
+                myReq.KeepAlive = true;
+                myReq.Headers.Add("Accept-Language", "zh-cn,en-us;q=0.5");
+                result = (HttpWebResponse)myReq.GetResponse();
+                Stream receviceStream = result.GetResponseStream();
+                StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+                strHTML = readerOfStream.ReadToEnd();
+                readerOfStream.Close();
+                receviceStream.Close();
+                result.Close();
+            }
+            catch
+            {
+                Uri uri = new Uri(url);
+                WebRequest webReq = WebRequest.Create(uri);
+                HttpWebRequest myReq = (HttpWebRequest)webReq;
+                myReq.UserAgent = "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705";
+                myReq.Accept = "*/*";
+                myReq.KeepAlive = true;
+                myReq.Headers.Add("Accept-Language", "zh-cn,en-us;q=0.5");
+                //result = (HttpWebResponse)myReq.GetResponse(); 
+                try
+                {
+                    result = (HttpWebResponse)myReq.GetResponse();
+                }
+                catch (WebException ex)
+                {
+                    result = (HttpWebResponse)ex.Response;
+                }
+                try
+                {
+                    Stream receviceStream = result.GetResponseStream();
+                    StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("gb2312"));
+                    strHTML = readerOfStream.ReadToEnd();
+                    readerOfStream.Close();
+                    receviceStream.Close();
+                    result.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("没有连接到NCUT！", "未连接");
+                    Console.WriteLine(ex.Message);
+
+                    pictureBox1.Hide();
+                    pictureBox2.Show();
+                    pictureBox3.Hide();
+
+                    //throw;
+                }
+
+
+            }
+            return strHTML;
+        }
+        static string GET(string input)
+        {
+            string result = "";
+            int temp = input.Length;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (char.IsNumber(input, temp - i))
+                {
+                    for (int j = input.Length - i; j >= 0; j--)
+                    {
+                        if (char.IsNumber(input, j))
+                        {
+                            result += input[j];
+                        }
+                        else
+                        {
+                            return result;
+                        }
+                    }
+                }
+
+            }
+            return result;
+        }
+        static string fanzhuan(string result)
+        {
+            string a = "";
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                a = a + result[i];
+            }
+            return a;
+        }
+        private void Fun()
+        {
+            string str;
+            this.textBox1.Text = "正在连接到NCUT，如果长时间没有反应，请检查网络连接！";
+            str = GetHttpWebRequest("http://192.168.254.251/");
+            this.textBox1.Text = "已经连接到NCUT(192.168.254.251)！";
+            string[] a = str.Split(';');
+            int res;
+            try
+            {
+                string b = a[54];
+                string result2;
+                result2 = GET(b);
+                result2 = fanzhuan(result2);
+                int.TryParse(result2, out res);
+                int flow, flow1, flow0;
+                flow = res;
+                flow0 = flow % 1024;
+                flow1 = flow - flow0;
+                flow0 = flow0 * 1000;
+                flow0 = flow0 - flow0 % 1024;
+                string flow3 = ".";
+                string detail;
+                detail = "已使用流量 : " + flow1 / 1024 + flow3 + flow0 / 1024 + " M";
+                this.textBox2.Text = detail;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                pictureBox1.Hide();
+                pictureBox2.Hide();
+                pictureBox3.Show();
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();
+            toolStripStatusLabel2.Text = "完成初始化。";
+            timer1.Start();
+        }
+
+
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "注意";
+            notifyIcon1.BalloonTipText = "双击托盘图标打开程序。";
+            notifyIcon1.ShowBalloonTip(3000);
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            toolStripStatusLabel2.Text = "正在进行间隔2s的刷新。";
+            Fun();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            if (IsPic)
+            {
+                pictureBox1.Hide();
+                IsPic = !IsPic;
+            }
+            else
+            {
+                pictureBox1.Show();
+                IsPic = !IsPic;
+            }
+            textBox1.Text += "\r\n";
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+            toolStripStatusLabel2.Text = "正在进行间隔3s的刷新。";
+            Fun();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            if (IsPic)
+            {
+                pictureBox1.Hide();
+                IsPic = !IsPic;
+            }
+            else
+            {
+                pictureBox1.Show();
+                IsPic = !IsPic;
+            }
+            textBox1.Text += "\r\n";
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "正在进行间隔5s的刷新。";
+            Fun();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            if (IsPic)
+            {
+                pictureBox1.Hide();
+                IsPic = !IsPic;
+            }
+            else
+            {
+                pictureBox1.Show();
+                IsPic = !IsPic;
+            }
+            textBox1.Text += "\r\n";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            toolStripStatusLabel2.Text = "正在进行间隔2s的刷新。";
+            timer2.Start();
+            timer4.Stop();
+            timer3.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            toolStripStatusLabel2.Text = "正在进行间隔3s的刷新。";
+            timer3.Start();
+            timer4.Stop();
+            timer2.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            toolStripStatusLabel2.Text = "正在进行间隔5s的刷新。";
+            timer4.Start();
+            timer2.Stop();
+            timer3.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            timer4.Stop();
+            timer3.Stop();
+            pictureBox1.Show();
+            pictureBox2.Show();
+            pictureBox3.Show();
+            toolStripStatusLabel2.Text = "您已经停止刷新。";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            string str;
+            textBox1.Text = "正在连接到NCUT，如果长时间没有反应，请检查网络连接！";
+            str = GetHttpWebRequest("http://192.168.254.251/");
+            //Console.WriteLine(str);
+            //MessageBox.Show(str);
+            textBox1.Text = str;
+            //           Regex r = new Regex("flow='");
+            string[] a = str.Split(';');
+            //           Match m = r.Match(str);
+            //           if (m.Success)
+            //           {
+            //               Console.WriteLine("Found match at position " + m.Index); //输入匹配字符的位置
+            //          }
+            int res;
+            try
+            {
+                string b = a[54];
+                string result2;
+                string result1;
+                result2 = GET(b);
+                result1 = fanzhuan(result2);
+                int.TryParse(result1, out res);
+                //res = int.Parse(b);
+                //MessageBox.Show(b);
+                int flow, flow1, flow0;
+                flow = res;
+                flow0 = flow % 1024;
+                flow1 = flow - flow0;
+                flow0 = flow0 * 1000;
+                flow0 = flow0 - flow0 % 1024;
+                string flow3 = ".";
+                string detail;
+                detail = "已使用流量 : " + flow1 / 1024 + flow3 + flow0 / 1024 + " M";
+                //MessageBox.Show(detail);
+                textBox2.Text = detail;
+                toolStripStatusLabel2.Text = "连接测试：已经连接到NCUT。";
+
+                pictureBox1.Hide();
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel2.Text = "连接测试：连接失败请检查网络是否连接，或者是否登陆NCUT！";
+                Console.WriteLine(ex.Message);
+                pictureBox1.Hide();
+                pictureBox2.Show();
+                pictureBox3.Hide();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+            pictureBox2.Show();
+            pictureBox3.Show();
+            System.Environment.Exit(0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
